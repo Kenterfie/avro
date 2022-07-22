2d8909002bb9e4508784bd6cec963e29ed774635
stefan.reusch <stefan.reusch@nitrado.net>
2022-07-22 13:38:16 +0200

    Revert "Adds new test case which cause a nil pointer dereference"
    This reverts commit 1cf87ef26d3d31f3807f184058dcec05c734262f.

--- decoder_record_test.go
+++ decoder_record_test.go
@@ -2,11 +2,11 @@ package avro_test
 
 import (
 	"bytes"
+	"testing"
+
 	"github.com/hamba/avro/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
-	"time"
 )
 
 func TestDecoder_RecordStruct(t *testing.T) {
@@ -73,56 +73,6 @@ func TestDecoder_RecordStructPtrNil(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, &TestRecord{A: 27, B: "foo"}, got)
-}
-
-func TestDecoder_TestStruct(t *testing.T) {
-	defer ConfigTeardown()
-
-	data := []byte{
-		128, 1, 69, 0, 0, 115, 212, 135, 0, 0,
-		56, 17, 225, 201, 109, 131, 253, 2, 195, 82,
-		158, 80, 9, 0, 44, 136, 0, 95, 38, 198,
-		12, 70, 20, 162, 112, 159, 180, 248, 156, 169,
-		62, 16, 107, 118, 137, 183, 11, 196, 10, 97,
-		78, 146, 66, 65, 189, 66, 94, 23, 26, 174,
-		30, 232, 67, 112, 162, 6, 128, 1, 0, 20,
-		83, 84, 83, 95, 65, 67, 67, 69, 80, 84,
-		60, 50, 48, 50, 50, 45, 48, 55, 45, 50,
-		48, 84, 49, 54, 58, 52, 51, 58, 53, 48,
-		46, 53, 54, 52, 49, 50, 53, 53, 54, 55,
-		90}
-	schema := `{
-	"type": "record",
-	"name": "packetInfo",
-	"namespace": "steelshield",
-	"fields" : [
-		{"name": "payload", "type": "bytes"},
-		{"name": "length", "type": "int"},
-		{"name": "comment", "type": "string"},
-		{"name": "action", "type": "string"},
-		{"name": "timestamp", "type":"string", "logicalType": "long.timestamp-millis"}
-	]
-}`
-	dec, err := avro.NewDecoder(schema, bytes.NewReader(data))
-	assert.NoError(t, err)
-
-	var got PacketInfoRecord
-	err = dec.Decode(&got)
-	assert.NoError(t, err)
-
-	assert.Equal(t, got.payload, []uint8{
-		0x45, 0x0, 0x0, 0x73, 0xd4, 0x87, 0x0, 0x0, 0x38, 0x11,
-		0xe1, 0xc9, 0x6d, 0x83, 0xfd, 0x2, 0xc3, 0x52, 0x9e, 0x50,
-		0x9, 0x0, 0x2c, 0x88, 0x0, 0x5f, 0x26, 0xc6, 0xc, 0x46,
-		0x14, 0xa2, 0x70, 0x9f, 0xb4, 0xf8, 0x9c, 0xa9, 0x3e, 0x10,
-		0x6b, 0x76, 0x89, 0xb7, 0xb, 0xc4, 0xa, 0x61, 0x4e, 0x92,
-		0x42, 0x41, 0xbd, 0x42, 0x5e, 0x17, 0x1a, 0xae, 0x1e, 0xe8,
-		0x43, 0x70, 0xa2, 0x6})
-	assert.Equal(t, got.action, "STS_ACCEPT")
-	assert.Equal(t, got.comment, "")
-	assert.Equal(t, got.length, 64)
-	packetTime, err := time.Parse(time.RFC3339, "2022-07-20T16:43:50Z")
-	assert.Equal(t, got.timestamp.Format(time.RFC3339), packetTime)
 }
 
 func TestDecoder_RecordStructWithFieldAlias(t *testing.T) {
